44780409b8c4125a3eefaeab270ced27eb30c20a
issmmaiill <99246224+issmmaiill@users.noreply.github.com>
2022-02-15 12:03:31 +0300

    Add files via upload

---
+++ WindowsFormsApplication10/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication10
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            int i = 1;
+            int s1 ;
+
+            while (i < Convert.ToInt32(textBox2.Text))
+                
+            {
+                listBox1.Items.Add(textBox1.Text);
+                s1 = Convert.ToInt32(textBox2.Text);
+                i++;
+
+            }
+        }
+    }
+}
